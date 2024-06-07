467f6f89ba082620c2b12e2cf2cb4b1363d42187
tranbinhminh1403 <myemail@example.com>
2024-06-07 16:49:36 +0700

    update: get orders by id

--- backend/src/main/java/com/sapo/edu/backend/controller/OrdersController.java
+++ backend/src/main/java/com/sapo/edu/backend/controller/OrdersController.java
@@ -2,6 +2,28 @@ package com.sapo.edu.backend.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sapo.edu.backend.model.Orders;
+import com.sapo.edu.backend.repository.OrdersRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
 @RestController
+@RequestMapping("/orders-management")
+
 public class OrdersController {
+    @Autowired
+    private OrdersRepository ordersRepository;
+
+
+    @GetMapping("/orders/{id}")
+    public List<Orders> getOrderById(@PathVariable("id") int id) {
+        return ordersRepository.getOrdersById(id);
+    }
 }
--- backend/src/main/java/com/sapo/edu/backend/repository/OrdersRepository.java
+++ backend/src/main/java/com/sapo/edu/backend/repository/OrdersRepository.java
@@ -1,9 +1,19 @@
 package com.sapo.edu.backend.repository;
 
 import com.sapo.edu.backend.model.Orders;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrdersRepository extends JpaRepository<Orders,Integer> {
+    @Query("SELECT o FROM Orders o WHERE o.orderId = :id")
+    public List<Orders> getOrdersById(@Param("id") int id);
+    // Page<Orders> findAllByOrderNameContainingIgnoreCase(String name, Pageable pageable);
 }
