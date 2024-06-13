1f18a5e13c859cf2e62c9ce2b14e021e2d2aef73
Nguyễn Quốc Hùng <107848000+HungNguyen20207677@users.noreply.github.com>
2024-06-13 18:59:53 +0700

    get shipper list with pagination

--- backend/src/main/java/com/sapo/edu/backend/repository/UsersRepository.java
+++ backend/src/main/java/com/sapo/edu/backend/repository/UsersRepository.java
@@ -1,11 +1,14 @@
 package com.sapo.edu.backend.repository;
 
 import com.sapo.edu.backend.model.Users;
+import com.sapo.edu.backend.model.enumclasses.Roles;
 import org.apache.catalina.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,5 +16,8 @@ public interface UsersRepository extends JpaRepository<Users, Integer> {
 
     // Searching the user by username
     Optional<Users> findByUsername(String username);
+
+    //Get list of users by role with pagination
+    List<Users> findAllByRole(Roles role, Pageable pageable);
 
 }
---
+++ backend/src/main/java/com/sapo/edu/backend/service/ShopsService.java
@@ -0,0 +1,13 @@
+package com.sapo.edu.backend.service;
+
+import com.sapo.edu.backend.model.Users;
+import com.sapo.edu.backend.model.enumclasses.Roles;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ShopsService {
+
+}
